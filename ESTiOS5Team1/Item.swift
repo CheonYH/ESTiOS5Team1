b5fafa0d78920dc1ade6a0d4b26e58e2f60fc0e8
cheon <cun76530285@gmail.com>
2026-01-06 10:11:09 +0900

    Initial Commit

---
+++ ESTiOS5Team1/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ESTiOS5Team1
+//
+//  Created by cheon on 1/6/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
