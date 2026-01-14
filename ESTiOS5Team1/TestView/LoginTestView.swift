f0826dd5208e2a8d3de3ec0ceef5ec5844f06d7e
cheon <cun76530285@gmail.com>
2026-01-14 13:37:48 +0900

    #include "/Users/cheon/Documents/Secret/IGDB.xcconfig"

---
+++ ESTiOS5Team1/TestView/LoginTestView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginTestView.swift
+//  ESTiOS5Team1
+//
+//  Created by cheon on 1/14/26.
+//
+
+import SwiftUI
+
+struct LoginTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginTestView()
+}
